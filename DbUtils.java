3a4c7f0d6d746193b1163b583dab6846b55e606d
Vikash Tiwari <86182391+vikashtiwari054@users.noreply.github.com>
2021-07-15 22:41:26 +0530

    Add files via upload

---
+++ DbUtils.java
@@ -0,0 +1,15 @@
+package hotel.management.system;
+
+import java.sql.ResultSet;
+
+import javax.swing.table.TableModel;
+
+public class DbUtils {
+
+	public static TableModel resultSetToTableModel(ResultSet rs) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+}
